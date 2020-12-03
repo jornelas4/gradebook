40fd568a5139baffa50d3d1072bf833f893cc343
jornelas4 <jornelas4@my.wctc.edu>
2020-12-02 22:03:45 -0600

    Added Sort by Name and Percentage

--- jquery.js
+++ jquery.js
@@ -1,6 +1,12 @@
+let studentArray = [];
+
 $(document).ready(function(){
 
+
     $("#submit").click(results);
+    $("#sortpercent").click(sortPercentage);
+    $("#sortName").click(sortName);
+
 
     function results(event) {
         event.preventDefault();
@@ -10,7 +16,8 @@ $(document).ready(function(){
                 firstName: "",
                 lastName: "",
                 pointsEarned: 0,
-                pointsPossible: 0
+                pointsPossible: 0,
+                calculation: 0
 
             };
 
@@ -25,6 +32,8 @@ $(document).ready(function(){
         $("#resultsOutput").text(`${possiblepoints.firstName} 
     ${possiblepoints.lastName} results are: ${calculation.toFixed()}%`);
 
+        possiblepoints.calculation = calculation;
+
         if (calculation < 60) {
             $('#letterOutput').text("C");
         } else if (calculation < 80) {
@@ -32,6 +41,74 @@ $(document).ready(function(){
         } else {
             $("#letterOutput").text("A");
         }
+
+        studentArray.push(possiblepoints);
+
+        var output = "";
+
+        for (var studentInfo of studentArray){
+
+            output += studentInfo.firstName;
+        }
+
+        $("#letterOutput").text(output);
+
+        var poutput = "";
+
+        for (var studentInfo of studentArray){
+
+            poutput += studentInfo.calculation;
+        }
+
+        $("#percentOutput").text(poutput);
+
     }
+
+    function sortPercentage() {
+        studentArray.sort(function (a, b) {
+            if (a.calculation < b.calculation) {
+                return -1;
+            } else if (a.calculation < b.calculation) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        let poutput = "";
+
+        for (let studentInfo of studentArray) {
+
+            poutput += studentInfo.calculation;
+
+        }
+
+        $("#percentOutput").text(poutput);
+    }
+
+    function sortName() {
+        studentArray.sort(function (a, b) {
+            if (a.firstName < b.firstName) {
+                return -1;
+            } else if (a.firstName < b.firstName) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        let output = "";
+
+        for (let studentInfo of studentArray) {
+
+            output += studentInfo.firstName + "  ";
+
+        }
+
+        $("#letterOutput").text(output);
+    }
+
+
+
 
 });
